e365f7abc23d16fba38041c0adbc85af0813cb93
Arvin A <aliento.arvin.b.0477@gmail.com>
2026-03-04 17:03:51 +0800

    fix: add typescript types for model-viewer

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,5 +1,17 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'model-viewer': any;
+import React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'model-viewer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        src?: string;
+        ar?: boolean;
+        'auto-rotate'?: boolean;
+        'camera-controls'?: boolean;
+        'shadow-intensity'?: string;
+        'environment-image'?: string;
+        exposure?: string;
+      };
+    }
   }
 }
